756cf34f8955f93a42cf00b71fd1a73763ffb6c3
MaDevops <mathieu.dussault@gmail.com>
2019-12-23 17:42:13 +0100

    V0.2

---
+++ js/script.js
@@ -0,0 +1,88 @@
+
+
+	function resized(){
+		
+		window_width = $( window ).width();
+		window_height = $( window ).height();
+
+        hauteurhp1 = $('#pratiques').outerHeight();
+        $('#imgpratiques').css('min-height', hauteurhp1);
+
+	}
+
+
+
+
+$( document ).ready( function() {
+
+resized();
+
+    
+
+});
+
+
+
+   	$( window ).resize(	function(){
+			//resized();
+		});
+
+	
+	$( window ).on( "orientationchange", function( event ) {
+		//resized();
+	});
+
+	
+
+  $('.back').css('-webkit-backface-visibility', 'hidden');
+ $('.back').css('backface-visibility', 'hidden');
+
+
+
+
+
+
+   
+
+   
+
+$('.fermercredits').on('click',function(){
+    $('.popup.credits').hide();
+
+});
+
+
+	   
+   	  // $(window).load(function(){
+           
+   // PAGE IS FULLY LOADED  
+   // FADE OUT YOUR OVERLAYING DIV
+		
+	/*	$('.container-loader').css('opacity', '0'); 
+		$('.container-loader').addClass('d-none ');
+		$('.loader').addClass('d-none ');
+		
+			$('header').removeClass('d-none');
+		$('#bienvenue').removeClass('d-none');
+		$('.navbar').removeClass('d-none');
+		$('footer').removeClass('d-none');
+		$('#hp').removeClass('d-none');
+
+		$('#cabinet').removeClass('nopacity');
+		$('#conseils').removeClass('nopacity');
+		$('#ortho').removeClass('nopacity');
+		$('#contact').removeClass('nopacity');
+		$('#urgence').removeClass('nopacity');
+		$('#patient').removeClass('nopacity');*/
+
+		//$('#sousmenuconseils').removeClass('d-none');
+
+
+		//resized();
+
+//});
+
+
+
+
+
